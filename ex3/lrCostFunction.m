516427faa612f8db90e90205eabe4f8b815da934
Pansiyuanxianren <psy1295497127@163.com>
2022-05-09 21:34:24 +0800

    there are some mistakes

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -35,13 +35,11 @@ grad = zeros(size(theta));
 %           temp(1) = 0;   % because we don't add anything for j = 0  
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
+h=sigmoid(X*theta);
 
+J = (-1*y'*log(h)-(1-y)' * log(1-h))/m + lambda/2/m*([0;theta(2:end)].^2);
 
-
-
-
-
-
+grad = X'*(h-y)/m+lambda/m*[0;theta(2:end)];
 
 
 
